5d9a1926fd68229277c5b68816d2a5c26d5d7e32
Kamaya7648 <kamayakapugamage7@gmail.com>
2026-08-23 09:46:07 +0530

    Add route for AddVehicle page

--- trello-clone/src/App.jsx
+++ trello-clone/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard.jsx'
 import Board from './pages/Board.jsx'
+import AddVehicle from './pages/AddVehicle.jsx'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/board/:vehicleId" element={<Board />} />
+      <Route path="/add-vehicle" element={<AddVehicle />} />
     </Routes>
   )
 }
