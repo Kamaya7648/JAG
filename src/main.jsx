062f749255ef6756dabe46f7ea8a73d227c1cf0d
Kamaya7648 <kamayakapugamage7@gmail.com>
2026-08-28 22:06:39 +0530

    Update import statements and file extensions in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { AppProvider } from "./context/AppContext";
-import App from "./App";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+import App from './App.jsx'
+import { AppProvider } from "./context/AppContext.jsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
     </BrowserRouter>
-  </StrictMode>
-);
+  </StrictMode>,
+)
