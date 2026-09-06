436eb91681965b9e10aa2e9cbd157b625f1a779b
OAhewavitharane <oshanihewavitharane@gmail.com>
2026-09-06 16:31:51 +0530

    Fix vehicle routes

--- backend/routes/vehicleRoutes.js
+++ backend/routes/vehicleRoutes.js
@@ -1,14 +1,19 @@
 const express = require("express");
+
 const {
   getVehicles,
   getVehicleById,
-  createVehicle,
+  addVehicle,
+  updateVehicle,
+  deleteVehicle
 } = require("../controllers/vehicleController");
 
 const router = express.Router();
 
 router.get("/", getVehicles);
 router.get("/:id", getVehicleById);
-router.post("/", createVehicle);
+router.post("/", addVehicle);
+router.put("/:id", updateVehicle);
+router.delete("/:id", deleteVehicle);
 
 module.exports = router;
